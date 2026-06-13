0f7663f2a53301715e571e61f8ddef8fd90fb48d
SplittyDev <splittydev@protonmail.com>
2026-06-14 05:58:55 +0700

    Implement log filtering by log level

--- DarkbloomDashboard/Views/Tabs/LogsTab.swift
+++ DarkbloomDashboard/Views/Tabs/LogsTab.swift
@@ -5,12 +5,98 @@ import FiveKit
 import FuzzySearch
 import OSLog
 
+enum LogLevelMapping: String, Identifiable, CaseIterable, Hashable {
+    case undefined
+    case debug
+    case info
+    case notice
+    case error
+    case fault
+    
+    var id: String { rawValue }
+    
+    var displayName: String {
+        switch self {
+            case .undefined: "Undefined"
+            case .debug: "Debug"
+            case .info: "Info"
+            case .notice: "Warning"
+            case .error: "Error"
+            case .fault: "Critical"
+        }
+    }
+    
+    var osLogLevel: OSLogEntryLog.Level {
+        switch self {
+            case .undefined: .undefined
+            case .debug: .debug
+            case .info: .info
+            case .notice: .notice
+            case .error: .error
+            case .fault: .fault
+        }
+    }
+    
+    var priority: Int {
+        switch self {
+            case .undefined: 0
+            case .debug: 1
+            case .info: 2
+            case .notice: 3
+            case .error: 4
+            case .fault: 5
+        }
+    }
+    
+    var includingHigherPriority: Set<LogLevelMapping> {
+        var set: Set<LogLevelMapping> = [self]
+        for logLevel in Self.allCases where logLevel.priority > self.priority {
+            set.insert(logLevel)
+        }
+        return set
+    }
+}
+
 struct LogsTab: View {
     @Environment(LocalLogController.self) private var viewModel
     
     @State private var searchText: String = ""
     @State private var searchTask: Task<Void, Never>?
     @State private var filteredLogs: [DarkbloomLogEntry]?
+    @State private var activeFilter: LogLevelMapping?
+    @State private var logLevelCounts: [OSLogEntryLog.Level: Int] = [:]
+    
+    private func recalculateLogLevelCounts() {
+        var logLevelCounts: [OSLogEntryLog.Level: Int] = [:]
+        for log in viewModel.logs {
+            logLevelCounts[log.level, default: 0] += 1
+        }
+        self.logLevelCounts = logLevelCounts
+    }
+    
+    private func reevaluateSearchResults() {
+        searchTask?.cancel()
+        searchTask = Task {
+            let searchableLogs = {
+                if let activeFilter {
+                    let eligibleLogLevels = activeFilter.includingHigherPriority.map(\.osLogLevel)
+                    return viewModel.logs.filter { log in
+                        eligibleLogLevels.contains(log.level)
+                    }
+                } else {
+                    return viewModel.logs
+                }
+            }()
+            guard !searchText.isEmpty else {
+                self.filteredLogs = searchableLogs
+                return
+            }
+            if Task.isCancelled { return }
+            let results = await Fuzzy().search(for: searchText, in: searchableLogs, minimumScore: 0.4)
+            if Task.isCancelled { return }
+            self.filteredLogs = results.map(\.item)
+        }
+    }
     
     var body: some View {
         Form {
@@ -50,15 +136,35 @@ struct LogsTab: View {
         }
         .formStyle(.grouped)
         .searchable(text: $searchText)
+        .onChange(of: viewModel.logs, initial: true) {
+            recalculateLogLevelCounts()
+        }
         .onChange(of: searchText) {
-            searchTask?.cancel()
-            guard !searchText.isEmpty else {
-                filteredLogs = nil
-                return
-            }
-            searchTask = Task {
-                let results = await Fuzzy().search(for: searchText, in: viewModel.logs)
-                self.filteredLogs = results.map(\.item)
+            reevaluateSearchResults()
+        }
+        .onChange(of: activeFilter) {
+            reevaluateSearchResults()
+        }
+        .toolbar {
+            Menu {
+                Picker("", selection: $activeFilter) {
+                    Text("Everything")
+                        .tag(nil as LogLevelMapping?)
+                    Divider()
+                    Section {
+                        ForEach(LogLevelMapping.allCases) { level in
+                            Text("\(level.displayName) (\(logLevelCounts[level.osLogLevel, default: 0]))")
+                                .tag(level as LogLevelMapping?)
+                        }
+                    } header: {
+                        Text("Minimum Log Level")
+                    }
+                }
+                .labelsHidden()
+                .pickerStyle(.inline)
+            } label: {
+                Image(systemName: "line.3.horizontal.decrease")
+                    .symbolVariant(activeFilter != nil ? .circle.fill : .circle)
             }
         }
         .onAppear {
